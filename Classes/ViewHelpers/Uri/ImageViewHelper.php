2b5ef9774b61f7a0017b8487e153bb8cf14631f0
s-leger <stephen@3dservices.ch>
2016-11-16 07:18:45 +0100

    8.4 compatibility

--- Classes/ViewHelpers/Uri/ImageViewHelper.php
+++ Classes/ViewHelpers/Uri/ImageViewHelper.php
@@ -64,49 +64,40 @@ use TYPO3\CMS\Fluid\Core\ViewHelper\Facets\CompilableInterface;
  * <output>
  * Could not get image resource for "NonExistingImage.png".
  * </output>
- *
- * add: quality and output format options 
  */
 class ImageViewHelper extends AbstractViewHelper implements CompilableInterface
 {
+      /**
+     * Initialize arguments
+     */
+    public function initializeArguments()
+    {
+        parent::initializeArguments();
+        $this->registerArgument('src', 'string', 'src');
+        $this->registerArgument('image', 'object', 'image');
+        $this->registerArgument('width', 'string', 'width of the image. This can be a numeric value representing the fixed width of the image in pixels. But you can also perform simple calculations by adding "m" or "c" to the value. See imgResource.width for possible options.');
+        $this->registerArgument('height', 'string', 'height of the image. This can be a numeric value representing the fixed height of the image in pixels. But you can also perform simple calculations by adding "m" or "c" to the value. See imgResource.width for possible options.');
+        $this->registerArgument('minWidth', 'int', 'minimum width of the image');
+        $this->registerArgument('minHeight', 'int', 'minimum height of the image');
+        $this->registerArgument('maxWidth', 'int', 'maximum width of the image');
+        $this->registerArgument('maxHeight', 'int', 'maximum height of the image');
+        $this->registerArgument('treatIdAsReference', 'bool', 'given src argument is a sys_file_reference record', false, false);
+        $this->registerArgument('crop', 'string|bool', 'overrule cropping of image (setting to FALSE disables the cropping set in FileReference)');
+        $this->registerArgument('absolute', 'bool', 'Force absolute URL', false, false);
+         $this->registerArgument('format', 'string', 'file format', false, null);
+        $this->registerArgument('quality', 'int', 'image quality', false, null);
+   }
     /**
      * Resizes the image (if required) and returns its path. If the image was not resized, the path will be equal to $src
      *
      * @see https://docs.typo3.org/typo3cms/TyposcriptReference/ContentObjects/ImgResource/
-     * @param string $src
-     * @param FileInterface|AbstractFileFolder $image
-     * @param string $width width of the image. This can be a numeric value representing the fixed width of the image in pixels. But you can also perform simple calculations by adding "m" or "c" to the value. See imgResource.width for possible options.
-     * @param string $height height of the image. This can be a numeric value representing the fixed height of the image in pixels. But you can also perform simple calculations by adding "m" or "c" to the value. See imgResource.width for possible options.
-     * @param int $minWidth minimum width of the image
-     * @param int $minHeight minimum height of the image
-     * @param int $maxWidth maximum width of the image
-     * @param int $maxHeight maximum height of the image
-     * @param bool $treatIdAsReference given src argument is a sys_file_reference record
-     * @param string|bool $crop overrule cropping of image (setting to FALSE disables the cropping set in FileReference)
-     * @param bool $absolute Force absolute URL
-     * @param string $format file format 
-     * @param int $quality quality of image
      * @throws Exception
      * @return string path to the image
      */
-    public function render($src = null, $image = null, $width = null, $height = null, $minWidth = null, $minHeight = null, $maxWidth = null, $maxHeight = null, $treatIdAsReference = false, $crop = null, $absolute = false, $format = null, $quality = null)
+    public function render()
     {
         return self::renderStatic(
-            [
-                'src' => $src,
-                'image' => $image,
-                'width' => $width,
-                'height' => $height,
-                'minWidth' => $minWidth,
-                'minHeight' => $minHeight,
-                'maxWidth' => $maxWidth,
-                'maxHeight' => $maxHeight,
-                'treatIdAsReference' => $treatIdAsReference,
-                'crop' => $crop,
-                'absolute' => $absolute,
-                'format' => $format,
-                'quality' => $quality 
-            ],
+            $this->arguments,
             $this->buildRenderChildrenClosure(),
             $this->renderingContext
         );
@@ -134,10 +125,12 @@ class ImageViewHelper extends AbstractViewHelper implements CompilableInterface
         try {
             $imageService = self::getImageService();
             $image = $imageService->getImage($src, $image, $treatIdAsReference);
+
             if ($crop === null) {
                 $crop = $image instanceof FileReference ? $image->getProperty('crop') : null;
             }
-            $processingInstructions = [
+      
+      $processingInstructions = [
                 'width' => $arguments['width'],
                 'height' => $arguments['height'],
                 'minWidth' => $arguments['minWidth'],
@@ -146,11 +139,11 @@ class ImageViewHelper extends AbstractViewHelper implements CompilableInterface
                 'maxHeight' => $arguments['maxHeight'],
                 'crop' => $crop,
                 'fileExtension' => $arguments['format'],
-            ];
-            if (0 < intval($arguments['quality'])) {
-                $quality = MathUtility::forceIntegerInRange($arguments['quality'], 10, 100, 75);
-                $processingInstructions['additionalParameters'] .= ' -quality ' . $quality;
-            }
+      ];
+      if (0 < intval($arguments['quality'])) {
+        $quality = MathUtility::forceIntegerInRange($arguments['quality'], 10, 100, 75);
+        $processingInstructions['additionalParameters'] .= ' -quality ' . $quality;
+      }
             $processedImage = $imageService->applyProcessingInstructions($image, $processingInstructions);
             return $imageService->getImageUri($processedImage, $absolute);
         } catch (ResourceDoesNotExistException $e) {
